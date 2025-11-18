b805fc33ef8048e52e059f3adedf103fb8a573ae
Just <grivten@yandex.ru>
2025-11-18 23:51:35 +0700

    recompiling for singleplayer compatibility

--- src/main/java/net/just_s/BoatItemFrameFixMod.java
+++ src/main/java/net/just_s/BoatItemFrameFixMod.java
@@ -1,16 +1,15 @@
 package net.just_s;
 
-import net.fabricmc.api.DedicatedServerModInitializer;
-
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BoatItemFrameFixMod implements DedicatedServerModInitializer {
+public class BoatItemFrameFixMod implements ModInitializer {
 	public static final String MOD_ID = "boatframefix";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitializeServer() {
+	public void onInitialize() {
 		LOGGER.info("Fix for MC154651 loaded.");
 	}
 }
